dccfaabf1e5a3a8f173f6c43609de6c1a306c083
patcharta <patcharamon.t@kgcorp.com>
2024-06-04 11:10:02 +0700

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,10 +1,7 @@
-import os
 import pyodbc
 import pandas as pd
 import streamlit as st
 from datetime import datetime
-import base64
-import io
 import time
 import pytz
 
@@ -12,52 +9,45 @@ import pytz
 st.set_page_config(layout="wide")
 
 # Function to check user credentials
+@st.cache_data
 def check_credentials(username, password):
     user_db = {
-        'user1': 'password1',
-        'user2': 'password2',
+        'nui': '1234',
+        'pan': '5678',
+        'sand': '9876',
+        'fai': '5432',
+        'io': '1234',
+        'dream': '5678',
+        'tan': '9876',
         'admin': 'adminpassword'
     }
-    return user_db.get(username) == password
-
-@st.cache_data
-def get_server_details(company):
-    if company == 'K.G. Corporation Co.,Ltd.':
-        return {
-            'server': '61.91.59.134',
-            'port': '1544', 
-            'db_username': 'sa',
-            'db_password': 'kg@dm1nUsr!',
-            'database': 'KGETEST'
-        }
-    elif company == 'The Chill Resort & Spa Co., Ltd.':
-        return {
-            'server': '61.91.59.134',
-            'port': '1544',
-            'db_username': 'sa',
-            'db_password': 'kg@dm1nUsr!',
-            'database': 'THECHILL'
-        }
-    return None
+    return user_db.get(username.lower()) == password
 
 @st.cache_data
 def get_connection_string(company):
-    details = get_server_details(company)
-    if details:
-        server = details['server']
-        port = details['port']
-        database = details['database']
-        db_username = details['db_username']
-        db_password = details['db_password']
-        return f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={db_username};PWD={db_password}'
-    return None
+    if company == 'K.G. Corporation Co.,Ltd.':
+        server = '61.91.59.134'
+        port = '1544'
+        db_username = 'sa'
+        db_password = 'kg@dm1nUsr!'
+        database = 'KGETEST'
+    elif company == 'The Chill Resort & Spa Co., Ltd.':
+        server = '61.91.59.134'
+        port = '1544'
+        db_username = 'sa'
+        db_password = 'kg@dm1nUsr!'
+        database = 'THECHILL'
+
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={db_username};PWD={db_password}'
+    return conn_str
 
 def save_to_database(product_data, conn_str):
     try:
+        remark = product_data.get('Remark', '')
         query = '''
         INSERT INTO ERP_COUNT_STOCK (
-            ID, LOGDATE, ENTERBY, ITMID, ITEMNAME, UNIT, REMARK, ACTUAL, INSTOCK
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ID, LOGDATE, ENTERBY, ITMID, ITEMNAME, UNIT, REMARK, ACTUAL, INSTOCK, WHCID
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         '''
         with pyodbc.connect(conn_str) as conn:
             cursor = conn.cursor()
@@ -65,10 +55,10 @@ def save_to_database(product_data, conn_str):
             max_id = cursor.fetchone()[0]
             new_id = max_id + 1
             data = [
-                new_id, product_data['Login_Time'], product_data['Enter_By'], 
+                new_id, product_data['Time'], product_data['Enter_By'], 
                 product_data['Product_ID'], product_data['Product_Name'], 
-                product_data['Purchasing_UOM'], product_data['Remark'], 
-                product_data['Total_Balance'], product_data['Quantity']
+                product_data['Purchasing_UOM'], remark, 
+                product_data['Quantity'], product_data['Total_Balance'], product_data['whcid']
             ]
             cursor.execute(query, data)
             conn.commit()
@@ -80,7 +70,7 @@ def save_to_database(product_data, conn_str):
 def load_data(selected_product_name, selected_whcid, conn_str):
     query_detail = '''
     SELECT
-        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, 
+        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL,  a.PHOTONAME,
         b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME,
         p.WHCID, w.NAME_TH AS WAREHOUSE_NAME, p.BATCH_NO, SUM(p.BALANCE) AS TOTAL_BALANCE
     FROM
@@ -97,7 +87,7 @@ def load_data(selected_product_name, selected_whcid, conn_str):
         a.ITMID + ' - ' + a.NAME_TH + ' - ' + a.MODEL = ? AND
         p.WHCID = ?
     GROUP BY
-        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL, a.PHOTONAME,
+        a.ITMID, a.NAME_TH, a.PURCHASING_UOM, a.MODEL,  a.PHOTONAME,
         b.BRAND_NAME, c.CAB_NAME, d.SHE_NAME, e.BLK_NAME,
         p.WHCID, w.NAME_TH, p.BATCH_NO
     '''
@@ -119,7 +109,7 @@ def fetch_products(company):
     return items_df.fillna('')
 
 def select_product(company):
-    st.subheader("ค้นหาสินค้า")
+    st.write("ค้นหาสินค้า")
     items_df = fetch_products(company)
     items_options = [None] + list(items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'])
     selected_product_name = st.selectbox("เลือกสินค้า:", options=items_options, key='selected_product')
@@ -128,51 +118,67 @@ def select_product(company):
         selected_item = items_df[items_df['ITMID'] + ' - ' + items_df['NAME_TH'] + ' - ' + items_df['MODEL'] == selected_product_name]
         selected_brand_name = selected_item['BRAND_NAME'].iloc[0] if not selected_item.empty else ""
         st.write(f"คุณเลือกสินค้า: {selected_product_name} - {selected_brand_name}")
+        st.markdown("---")
         return selected_product_name, selected_item
     else:
         return None, None
 
 def count_product(selected_product_name, selected_item, conn_str):
     filtered_items_df = load_data(selected_product_name, st.session_state.selected_whcid, conn_str)
+    total_balance = 0  # Ensure total_balance is defined
+
     if not filtered_items_df.empty:
-        st.write("### รายละเอียดสินค้า:")
+        st.write("รายละเอียดสินค้า:")
+        # Combine the columns 'CAB_NAME', 'SHE_NAME', and 'BLK_NAME' into a single column
+        filtered_items_df['Location'] = filtered_items_df[['CAB_NAME', 'SHE_NAME', 'BLK_NAME']].apply(lambda x: ' / '.join(x.astype(str)), axis=1)
         filtered_items_df_positive_balance = filtered_items_df[filtered_items_df['TOTAL_BALANCE'] > 0]
+        
         if not filtered_items_df_positive_balance.empty:
-            display_columns = ['CAB_NAME', 'SHE_NAME', 'BLK_NAME', 'BATCH_NO', 'TOTAL_BALANCE']
+            display_columns = ['Location', 'BATCH_NO', 'TOTAL_BALANCE']
             st.dataframe(filtered_items_df_positive_balance[display_columns])
             total_balance = filtered_items_df_positive_balance['TOTAL_BALANCE'].sum()
             st.write(f"รวมยอดสินค้าในคลัง: {total_balance}")
-
-            product_quantity = st.number_input(label='จำนวนสินค้า 🛒', min_value=0, value=st.session_state.product_quantity)
-            remark = st.text_area('Remark', value=st.session_state.remark)
-            if st.button('👉 Enter') and product_quantity > 0:
-                product_data = {
-                    'Login_Time': st.session_state.login_time,
-                    'Enter_By': st.session_state.username,
-                    'Product_ID': str(filtered_items_df['ITMID'].iloc[0]),
-                    'Product_Name': str(filtered_items_df['NAME_TH'].iloc[0]),
-                    'Model': str(filtered_items_df['MODEL'].iloc[0]),
-                    'Brand_Name': str(filtered_items_df['BRAND_NAME'].iloc[0]),
-                    'Cabinet': str(filtered_items_df['CAB_NAME'].iloc[0]),
-                    'Shelf': str(filtered_items_df['SHE_NAME'].iloc[0]),
-                    'Block': str(filtered_items_df['BLK_NAME'].iloc[0]),
-                    'Warehouse_ID': str(filtered_items_df['WHCID'].iloc[0]),
-                    'Warehouse_Name': str(filtered_items_df['WAREHOUSE_NAME'].iloc[0]),
-                    'Batch_No': str(filtered_items_df['BATCH_NO'].iloc[0]),
-                    'Purchasing_UOM': str(filtered_items_df['PURCHASING_UOM'].iloc[0]),
-                    'Total_Balance': int(total_balance),
-                    'Quantity': int(product_quantity),
-                    'Remark': remark
-                }
-                st.session_state.product_data.append(product_data)
-                save_to_database(product_data, conn_str)
-                st.session_state.product_data = []
-                st.session_state.product_quantity = 0
-                st.session_state.remark = ""
+            
         else:
             st.write("ไม่มีสินค้าที่มียอดเหลือในคลัง")
+
     else:
         st.warning("ไม่พบข้อมูลสินค้าที่เลือก")
+
+    # Enable quantity and remark input even if there are no products with positive balance
+    product_quantity = st.number_input(label='จำนวนสินค้า 🛒', min_value=0, value=st.session_state.product_quantity)
+    remark = st.text_area('Remark', value=st.session_state.remark)
+    st.markdown("---")
+    if st.button('👉 Enter') and product_quantity > 0:
+        timezone = pytz.timezone('Asia/Bangkok')
+        current_time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+        product_data = {
+            'Time': current_time,
+            'Enter_By': st.session_state.username,
+            'Product_ID': str(filtered_items_df['ITMID'].iloc[0] if not filtered_items_df.empty else selected_item['ITMID'].iloc[0]),
+            'Product_Name': str(filtered_items_df['NAME_TH'].iloc[0] if not filtered_items_df.empty else selected_item['NAME_TH'].iloc[0]),
+            'Model': str(filtered_items_df['MODEL'].iloc[0] if not filtered_items_df.empty else selected_item['MODEL'].iloc[0]),
+            'Brand_Name': str(filtered_items_df['BRAND_NAME'].iloc[0] if not filtered_items_df.empty else selected_item['BRAND_NAME'].iloc[0]),
+            'Cabinet': str(filtered_items_df['CAB_NAME'].iloc[0] if not filtered_items_df.empty else ""),
+            'Shelf': str(filtered_items_df['SHE_NAME'].iloc[0] if not filtered_items_df.empty else ""),
+            'Block': str(filtered_items_df['BLK_NAME'].iloc[0] if not filtered_items_df.empty else ""),
+            'Warehouse_ID': str(filtered_items_df['WHCID'].iloc[0] if not filtered_items_df.empty else st.session_state.selected_whcid.split(' -')[0]),
+            'Warehouse_Name': str(filtered_items_df['WAREHOUSE_NAME'].iloc[0] if not filtered_items_df.empty else st.session_state.selected_whcid.split(' -')[1]),
+            'Batch_No': str(filtered_items_df['BATCH_NO'].iloc[0] if not filtered_items_df.empty else ""),
+            'Purchasing_UOM': str(filtered_items_df['PURCHASING_UOM'].iloc[0] if not filtered_items_df.empty else selected_item['PURCHASING_UOM'].iloc[0]),
+            'Total_Balance': int(total_balance) if not filtered_items_df.empty else 0,
+            'Quantity': int(product_quantity),
+            'Remark': remark,
+            'whcid': filtered_items_df['WHCID'].iloc[0] if not filtered_items_df.empty else st.session_state.selected_whcid.split(' -')[0]
+        }
+        st.session_state.product_data.append(product_data)
+        save_to_database(product_data, conn_str)
+        st.session_state.product_data = []
+        st.session_state.product_quantity = 0
+        st.session_state.remark = ""
+        time.sleep(2)
+        del st.session_state['selected_product']
+        st.experimental_rerun()
 
 def login_section():
     st.write("## 👾👾 Login")
@@ -181,13 +187,13 @@ def login_section():
     company_options = ['K.G. Corporation Co.,Ltd.', 'The Chill Resort & Spa Co., Ltd.']
     company = st.selectbox("Company", options=company_options)
     if st.button("Login"):
-        if check_credentials(username, password):
+        # Set the selected company to the session state
+        st.session_state.company = company
+        # Get the connection string based on the selected company
+        conn_str = get_connection_string(company)
+        if check_credentials(username, password):  # Corrected call
             st.session_state.logged_in = True
             st.session_state.username = username
-            timezone = pytz.timezone('Asia/Bangkok')
-            current_time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
-            st.session_state.login_time = current_time
-            st.session_state.company = company
             st.success(f"🎉🎉 Welcome {username}")
             time.sleep(1)
             st.experimental_rerun()
@@ -195,11 +201,11 @@ def login_section():
             st.error("Invalid username or password")
 
 def main_section():
-    st.write(f"👨🏻‍💼👩🏻‍💼 รายการสินค้าที่ {st.session_state.username} นับ")
-    st.write({st.session_state.company})
+    st.write(f"👨🏻‍💼👩🏻‍💼 รายการสินค้าที่ {st.session_state.username.upper()} นับ")
+    st.write(st.session_state.company)
 
     if st.session_state.selected_whcid is None:
-        st.subheader("เลือก WHCID")
+        st.write("เลือก WHCID")
         conn_str = get_connection_string(st.session_state.company)
         with pyodbc.connect(conn_str) as conn:
             whcid_query = '''
@@ -213,6 +219,7 @@ def main_section():
                 st.session_state.selected_whcid = selected_whcid
     else:
         st.write(f"คุณเลือก WHCID: {st.session_state.selected_whcid}")
+        st.markdown("---")
         selected_product_name, selected_item = select_product(st.session_state.company)
         if selected_product_name:
             conn_str = get_connection_string(st.session_state.company)
@@ -220,7 +227,6 @@ def main_section():
         if st.button('📤 Logout'):
             st.session_state.logged_in = False
             st.session_state.username = ''
-            st.session_state.login_time = ''
             st.session_state.selected_whcid = None
             st.session_state.selected_product_name = None
             st.session_state.product_data = []
@@ -231,7 +237,6 @@ def app():
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
         st.session_state.username = ''
-        st.session_state.login_time = ''
         st.session_state.selected_whcid = None
         st.session_state.selected_product_name = None
         st.session_state.product_data = []
@@ -241,7 +246,7 @@ def app():
     if st.session_state.logged_in:
         main_section()
     else:
-        login_section()
+        login_section()  
 
 if __name__ == "__main__":
     app()
